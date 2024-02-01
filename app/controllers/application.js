ace00ea9fba761b30cbc1a5189539666a9ecbbcc
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-02-01 18:00:06 -0500

    Example of how to test with unhandledrejection

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,24 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
-import { resolve } from 'rsvp';
-import { run } from '@ember/runloop';
 
 export default class ApplicationController extends Controller {
   @service('my-service') myService;
   @service('flash-message') flashMessageService;
 
-  // --------- Want to write this ---------
-
-  // @action
-  // async doSomething() {
-  //   await this.myService.doSomething();
-  // }
-
-  // ---------  But have to write this ---------
-
   @action
-  doSomething() {
-    return resolve(this.myService.doSomething());
+  async doSomething() {
+    await this.myService.doSomething();
   }
 }
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,5 +7,14 @@ module.exports = function (defaults) {
     // Add options here
   });
 
-  return app.toTree();
+  // return app.toTree();
+  // Using embroider so sourcemaps work
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    packagerOptions: {
+      webpackConfig: {
+        devtool: 'source-map', // highest fidelity (and slowest)
+      },
+    },
+  });
 };
